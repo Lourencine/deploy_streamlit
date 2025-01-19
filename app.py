686da0f7e206a5f8c51550bbbd38f4d3bd4b6e7c
Diego Lourencine <dlourencine@gmail.com>
2025-01-19 14:48:21 -0300

    Commit streamlit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.title(' Aula 7 - Meu primeiro deploy')
+
+# Cria uma barra de seleção
+slider = st.slider("Selecione o ano: ", min_value=1995, max_value=2024)
+st.write(f'O ano selecionado foi: {slider}')
+
+# Cria uma barra de seleção podendo seleciona os anos que vão aparecer
+st.select_slider("Selecione o ano", options=[1995, 2015, 2023, 2024, 2025])
